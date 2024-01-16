94db94328bc8fc9bd7f30eac9fe94d17682255e5
Ivan Robol <Ivan.Robol@Marconi.loc>
2024-01-16 16:26:25 +0100

    fine dei test

--- src/app/elenco/elenco.component.ts
+++ src/app/elenco/elenco.component.ts
@@ -8,13 +8,20 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class ElencoComponent {
   @Input() titolo: string | undefined
   @Input() test: string | undefined
+  @Input() valCount: number | undefined
   @Output() eventoContatore=new EventEmitter<number>();
-  contatore : number = 0;
+  contatore : number = 0; 
   inc(){ // incremento di una variabile con un button
+    if (this.valCount!==undefined && this.valCount !== this.contatore) {
+      this.contatore =this.valCount;
+    }
     this.contatore++;
     this.eventoContatore.emit(this.contatore)
   }
   dec(){ // decremento di una variabile con un button
+    if (this.valCount!==undefined && this.valCount !== this.contatore) {
+      this.contatore =this.valCount;
+    }
     this.contatore--;
     this.eventoContatore.emit(this.contatore)
   }
