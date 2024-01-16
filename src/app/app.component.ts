ee2889c9a0a610844888b02b3c8a51900cd94fd4
Ivan Robol <Ivan.Robol@Marconi.loc>
2024-01-16 16:54:54 +0100

    button che cambia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'primoEsempio';
   contatoreGlobale: number=0;
+  visibile: boolean = true;
+  testo_button: string = 'Nascondi'
   eventoContatore(valore: number){
     this.contatoreGlobale = valore
   }
+  mod_visibile(){
+    if (this.visibile==true){
+      this.testo_button = 'Mostra'
+      this.visibile=false
+    }else{
+      this.testo_button = 'Nascondi'
+      this.visibile=true
+    }
+  }
+
 }
